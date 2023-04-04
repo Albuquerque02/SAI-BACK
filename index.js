6b97d5c57717e5159fe551b0cc8f1f0c53a122d2
albuquerque02 <felipe.albuquerque123@gmail.com>
2023-04-03 21:14:40 -0300

    Criado server e conectando como banco

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const sql = require("mssql");
+const config = {
+    user: "sa",
+    password: "123456",
+    server: "localhost",
+    port: 1433,
+    database: "sai",
+    options: {
+      trustedconnection: true,
+      enableArithPort: true,
+      instancename: "",
+      trustServerCertificate: true,
+    },
+};
+
+sql
+  .connect(config)
+  .then(console.log("Conectado ao banco sai"))
+  .catch((err) => console.log(err));
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log('Servidor iniciado em http://localhost:3000');
+});
